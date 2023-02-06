e5655d52c39e9913ef682996b882a7615aa55693
tbukic <contact@tome.8shield.net>
2023-02-06 03:34:53 +0100

    HW2 done.

--- Week2/homework/parametrized_etl_web_to_gcs.py
+++ Week2/homework/parametrized_etl_web_to_gcs.py
@@ -40,7 +40,7 @@ def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
     Write DataFrame out locally as parquet file.
     """
     path = Path(f'data/{color}/{dataset_file}.parquet')
-    path.parent.mkdir(exist_ok=True)
+    path.parent.mkdir(exist_ok=True, parents=True)
     df.to_parquet(path, compression='gzip')
     return path
 
